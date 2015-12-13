64d1d3087cd92c7a2bc88c381a06989d30950ab5
hawncho <hawncho@gmail.com>
2015-12-13 03:44:53 -0500

    Added name color changing (!color) and a basic !help command.

--- dirtybot.js
+++ dirtybot.js
@@ -1,5 +1,8 @@
 var Discord = require("discord.js");
- 
+
+// list of valid role colors
+var validColors = ["green", "emerald", "blue", "cyan", "indigo", "violet", "red", "magenta", "gold", "yellow", "redorange", "orange"];
+
 // Get the email and password
 var authDetails = require("./auth.json");
  
@@ -12,7 +15,6 @@ dirtyBot.on("ready", function () {
 
 // when the bot disconnects
 dirtyBot.on("disconnected", function () {
-	// alert the console
 	console.log("Disconnected!");
 
 	// exit node.js with an error
@@ -21,14 +23,110 @@ dirtyBot.on("disconnected", function () {
 
 // when the bot receives a message
 dirtyBot.on("message", function(msg){
-	//if message is "ping"
-	if(msg.content === "ping") {
-		// send a message to the channel the ping message was sent in
-		dirtyBot.reply(msg, "pong");
+	// show a list of all available commands
+	if (msg.content === "!help") {
+		dirtyBot.sendMessage(msg.channel, 
+			"\n" +
+			"!help - Show a list of all commands.\n" +
+			"!color <color> - Set your name color."
+		);
+	
+	// add the user to the role with a specified color
+	} else if (msg.content.indexOf("!color") === 0) {
+		var color = msg.content.split(" ")[1];
 		
-		// alert the console
-		console.log("pong-ed " + msg.author.username);
+		setUserColor(msg.author, color, msg.channel);
 	}
 });
- 
+
+function displayAllAvailableColors(channel) {
+	var nameColorString = "\nAvailable name colors: \n";
+	
+	for (var i = 0; i < validColors.length; i++) {
+		nameColorString += validColors[i] + "\n";
+	}
+	
+	nameColorString += "Use \"!color reset\" to remove all colors from your name."
+
+	dirtyBot.sendMessage(channel, nameColorString);
+}
+
+function setUserColor(user, color, channel) {
+	// delay before adding the new role
+	// since trying to do both without waiting for a removal command to finish
+	// causes the later commands to fail
+	const waitTimeForRemovals = 300;
+	
+	if (color == null) {
+		return displayAllAvailableColors(channel);
+	}
+	
+	color = color.toLowerCase();
+	
+	if (color === "reset") {
+		return removeAllColorRoles(user, channel.server);
+	}
+	
+	if (validColors.indexOf(color) !== -1) {
+		removeAllColorRoles(user, channel.server);
+	
+		var role = channel.server.roles.get("name", "@" + color);
+		
+		// if there are existing color roles to remove,
+		// wait for them to be removed before adding the new role
+		if (getColorRole(user, channel.server)) {
+			setTimeout( function () {
+				dirtyBot.addMemberToRole(user, role)
+			}, waitTimeForRemovals);
+		} else {
+			dirtyBot.addMemberToRole(user, role)
+		}
+		
+		console.log("set color of " + user.username + " to " + color);
+	} else {
+		return displayAllAvailableColors(channel);
+	}
+}
+
+function removeAllColorRoles(user, server) {
+	const timeBetweenRemovals = 200;
+
+	var colorRole = getColorRole(user, server);
+	
+	// continue attempting to remove color roles until all are removed
+	if (colorRole) {
+		removeUserFromRole(user, colorRole);
+		
+		setTimeout( function () {
+			removeAllColorRoles(user, server);
+		}, timeBetweenRemovals);
+	} else {
+		console.log("removed all colors from " + user.username);
+	}
+}
+
+function removeUserFromRole(user, role) {
+	dirtyBot.removeMemberFromRole(user, role)
+
+	console.log("removed user " + user.username + " from role " + role.name);
+}
+
+function getColorRole(user, server) {
+	var userRoles = server.rolesOfUser(user);
+	var role;
+	for (var i = 0; i < userRoles.length; i++) {
+		role = userRoles[i];
+		if (isColorRole(role)) return role;
+	}
+	return null;
+}
+
+function isColorRole(role) {
+	var color;
+	for (var i = 0; i < validColors.length; i++) {
+		color = validColors[i];
+		if (role.name === "@" + color) return true;
+	}
+}
+
 dirtyBot.login(authDetails.email, authDetails.password);
