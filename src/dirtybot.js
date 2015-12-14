ceb08d32c330153b6db4edc66b56379afcac7687
hawncho <hawncho@gmail.com>
2015-12-14 17:20:09 -0500

    Added IP field for HTTP server.

--- src/dirtybot.js
+++ src/dirtybot.js
@@ -5,7 +5,7 @@ http.createServer(function (request, response) {
 	response.writeHead(200, { "Content-Type": "text/plain" });
 	response.write("DirtyBot is running!\n");
 	response.end();
-}).listen(process.env.OPENSHIFT_NODEJS_PORT || 8080);
+}).listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 // Get the email and password
 var authDetails = require("../auth.json");
